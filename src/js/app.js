98611a3eb4b91f0d3adf5208f5fc71b491148136
xandro2021 <xandrocastro2021@gmail.com>
2025-12-05 21:29:31 -0600

    agregar preferencia usuario darkmode

--- src/js/app.js
+++ src/js/app.js
@@ -5,10 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function darkMode() {
 
+  const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+  if (prefiereDarkMode.matches) {
+    document.body.classList.add('dark-mode');
+  }
+  else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  prefiereDarkMode.addEventListener('change', function () {
+    if (prefiereDarkMode.matches) {
+      document.body.classList.add('dark-mode');
+    }
+    else {
+      document.body.classList.remove('dark-mode');
+    }
+  })
+
   const botonDarkMode = document.querySelector(".dark-mode-boton");
 
   botonDarkMode.addEventListener('click', () => document.body.classList.toggle("dark-mode"));
- 
+
 }
 
 function eventListeners() {
